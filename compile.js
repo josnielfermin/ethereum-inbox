1ebd66ea15309725165b5a73d7bc3589348fff14
josniel <josnieldavidfb18@gmail.com>
2025-10-22 17:54:55 -0400

    avance

--- compile.js
+++ compile.js
@@ -5,4 +5,6 @@ const solc = require("solc");
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
 const source = fs.readFileSync(inboxPath, "utf8");
 
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[":Inbox"];
+
+// console.log(solc.compile(source, 1));
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,8 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
+const assert = require("assert");
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+
+const web3 = new Web3(ganache.provider());
 
 // updated ganache and web3 imports added for convenience
 
