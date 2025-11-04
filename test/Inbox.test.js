a211fcabaaadeff86018cd055935eab34c8643e7
josniel <josnieldavidfb18@gmail.com>
2025-11-04 17:56:18 -0400

    advance

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,6 +4,28 @@ const { Web3 } = require("web3");
 
 const web3 = new Web3(ganache.provider());
 
-// updated ganache and web3 imports added for convenience
+// class Car {
+//   park() {
+//     return "stopped";
+//   }
 
-// contract test code will go here
+//   drive() {
+//     return "vroom";
+//   }
+// }
+
+// describe("Car", () => {
+//   let car;
+
+//   beforeEach(() => {
+//     car = new Car();
+//   });
+
+//   it("can park", () => {
+//     assert.strictEqual(car.park(), "stopped");
+//   });
+
+//   it("can drive", () => {
+//     assert.strictEqual(car.drive(), "vroom");
+//   });
+// });
