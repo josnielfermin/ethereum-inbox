55317db1dafaf3d7e953f213375e9f3144796046
josniel <josnieldavidfb18@gmail.com>
2025-11-05 17:52:09 -0400

    advances

--- deploy.js
+++ deploy.js
@@ -1,5 +1,20 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { interface, bytecode } = require("./compile");
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+  "shy green gloom chuckle shift uncover cave ten tuna void actor erupt",
+  "https://sepolia.infura.io/v3/85ba41084f1b41ae82dafd7bed22c7c2"
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log("first", accounts[0]);
+
+  await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["MuninnDev"] })
+    .send({ from: accounts[0], gas: "1000000" });
+};
+deploy();
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,8 +1,40 @@
 const assert = require("assert");
 const ganache = require("ganache");
 const { Web3 } = require("web3");
+const { interface, bytecode } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
+
+describe("Inbox", () => {
+  let accounts;
+  let inbox;
+
+  beforeEach(async () => {
+    accounts = await web3.eth.getAccounts();
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ["MuninnDev"] })
+      .send({ from: accounts[0], gas: "1000000" });
+  });
+
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.strictEqual(message, "MuninnDev");
+  });
+
+  it("can change the message", async () => {
+    await inbox.methods
+      .setMessage("0xMuninn")
+      .send({ from: accounts[0], gas: "1000000" });
+    const message = await inbox.methods.message().call();
+    assert.notStrictEqual(message, "MuninnDev");
+    assert.strictEqual(message, "0xMuninn");
+  });
+});
 
 // class Car {
 //   park() {
